6ffdc7335975d414ad54f22931dc7941ae02a719
Steven Lisboa <elstevo92@gmail.com>
2021-12-17 13:52:10 -0500

    Updates

---
+++ precache-manifest.d2d4caa0a644a78a1e27b24a724d58e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c803255ea417888d8772f06d86437a6",
+    "url": "/REPO_NAME/index.html"
+  },
+  {
+    "revision": "c8b11eccff2793907a9c",
+    "url": "/REPO_NAME/static/css/main.292ce709.chunk.css"
+  },
+  {
+    "revision": "f058380acfde14e1f9be",
+    "url": "/REPO_NAME/static/js/2.0d2e56b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/REPO_NAME/static/js/2.0d2e56b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b11eccff2793907a9c",
+    "url": "/REPO_NAME/static/js/main.e50684bd.chunk.js"
+  },
+  {
+    "revision": "8a7780112896418a5d3f",
+    "url": "/REPO_NAME/static/js/runtime-main.267e13a2.js"
+  }
+]);
